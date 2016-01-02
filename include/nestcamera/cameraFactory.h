6eef00eebe23d1ad96bf3e037f48861e2122c735
fullerth <fullerth@gmail.com>
2016-01-02 22:17:29 +0000

    Add include guards to cameraFactory.h

--- include/nestcamera/cameraFactory.h
+++ include/nestcamera/cameraFactory.h
@@ -1,7 +1,12 @@
 #include "nestcamera/cameraAdapter.h"
 
+#ifndef __NEST_CAMERA_CAMERA_FACTORY__
+#define __NEST_CAMERA_CAMERA_FACTORY__
+
 class CameraFactory {
     public:
         CameraAdapter getCamera() {
         }
 };
+
+#endif //__NEST_CAMERA_CAMERA_FACTORY__
