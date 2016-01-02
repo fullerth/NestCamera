8fd42348c682593cd4bc1d9a9699ed653eeb54ed
fullerth <fullerth@gmail.com>
2016-01-02 20:25:06 +0000

    Add testcameraFactory test module

---
+++ test/testcameraFactory/testcameraFactory.cxx
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "raspicam/raspicam.h"
+
+//class MockCameraAdapter : public cameraAdapter {
+//
+//};
+
+TEST(cameraFactory, hasGetCameraMethod) {
+    EXPECT_EQ(0, 1) << "Implement the test";
+} 
+
