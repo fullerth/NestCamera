2eefeb2f688a13665098c29eb4e95e2794113135
Thomas <fullerth@gmail.com>
2016-01-19 14:53:42 -0800

    Improved sshfs mounting of the pi

--- mountpi.sh
+++ mountpi.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-if [ ! -d "/mnt/pisquanch" ]
-then
-    sudo mkdir /mnt/pisquanch
+if [ "$1" = "mount" ] ; then
+    if [ ! -d "/mnt/pisquanch" ] ; then
+        sudo mkdir /mnt/pisquanch
+    fi
+    sudo sshfs -o IdentityFile=~/.ssh/id_rsa pi@pisquanch:/ /mnt/pisquanch/
+elif [ "$1" = "unmount" ] ; then
+    sudo umount -d /mnt/pisquanch
+else
+    echo "Use mount or unmount"
 fi
 
-sudo sshfs -o IdentityFile=~/.ssh/id_rsa pi@pisquanch:/ /mnt/pisquanch/
