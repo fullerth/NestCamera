581f264e31f519400af9d1465791f4b33ff4eba4
Thomas <fullerth@gmail.com>
2016-01-27 20:18:04 -0800

    Add username/hostname variables to CMakeLists. Fix mountpi for the nest

--- mountpi.sh
+++ mountpi.sh
@@ -4,7 +4,7 @@ if [ "$1" = "mount" ] ; then
     if [ ! -d "/mnt/pisquanch" ] ; then
         sudo mkdir /mnt/pisquanch
     fi
-    sudo sshfs -o IdentityFile=~/.ssh/id_rsa pi@pisquanch:/ /mnt/pisquanch/
+    sudo sshfs -o IdentityFile=~/.ssh/id_rsa pi@10.1.10.132:/ /mnt/pisquanch/
 elif [ "$1" = "unmount" ] ; then
     sudo umount -d /mnt/pisquanch
 else
