d48c18743ae1f3873ac25bdec2a42954876a45a7
Thomas <fullerth@gmail.com>
2016-01-31 13:01:01 -0800

    Add mockRaspiCam to library. File must still be installed to /usr/local/include manually

---
+++ lib/raspicam/src/mocks/mockRaspiCam.h
@@ -0,0 +1,32 @@
+#include "gmock/gmock.h"
+#include "raspicam/raspicam.h"
+
+namespace raspicam {
+
+    class MockRaspiCam : public RaspiCam {
+        public:
+            MOCK_METHOD1(open,
+                    bool(bool));
+            MOCK_METHOD0(startCapture,
+                    bool());
+            MOCK_CONST_METHOD0(isOpened,
+                    bool());
+            MOCK_METHOD0(grab,
+                    bool());
+            MOCK_METHOD2(retrieve,
+                    void(char, RASPICAM_FORMAT));
+            MOCK_CONST_METHOD0(getImageBufferData,
+                    unsigned char*());
+            MOCK_METHOD1(setWidth,
+                    void(unsigned int));
+            MOCK_METHOD1(setHeight,
+                    void(unsigned int));
+            MOCK_METHOD1(setHorizontalFlip,
+                    void(bool));
+            MOCK_METHOD1(setVerticalFlip,
+                    void(bool));
+                    
+    };
+
+}  // namespace raspicam
+
