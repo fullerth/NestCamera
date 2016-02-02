05f8fc451cc6395676452ffac99ea15521effbf1
Thomas <fullerth@gmail.com>
2016-02-02 13:48:43 -0800

    Move PI_USERNAME and PI_HOSTNAME into local configuration files

--- mountpi.sh
+++ mountpi.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+#Local file must define pi_username and pi_hostname
+source LocalPiInfo.sh
+
 if [ "$1" = "mount" ] ; then
     if [ ! -d "/mnt/pisquanch" ] ; then
         sudo mkdir /mnt/pisquanch
     fi
-    sudo sshfs -o IdentityFile=~/.ssh/id_rsa pi@pisquanch:/ /mnt/pisquanch/
+    sudo sshfs -o IdentityFile=~/.ssh/id_rsa ${pi_username}@${pi_hostname}:/ /mnt/pisquanch/
 elif [ "$1" = "unmount" ] ; then
     sudo umount -d /mnt/pisquanch
 else
