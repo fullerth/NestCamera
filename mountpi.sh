6d7dbdbdc896386b61590191d36114f3ff6f6a47
Thomas <fullerth@gmail.com>
2016-02-11 11:02:33 -0800

    Add clean option to mountpi.sh

--- mountpi.sh
+++ mountpi.sh
@@ -10,7 +10,9 @@ if [ "$1" = "mount" ] ; then
     sudo sshfs -o IdentityFile=~/.ssh/id_rsa ${pi_username}@${pi_hostname}:/ /mnt/pisquanch/
 elif [ "$1" = "unmount" ] ; then
     sudo umount -d /mnt/pisquanch
+elif [ "$1" = "clean" ] ; then
+    sudo rm -r /mnt/pisquanch
 else
-    echo "Use mount or unmount"
+    echo "Use mount, unmount or clean"
 fi
 
